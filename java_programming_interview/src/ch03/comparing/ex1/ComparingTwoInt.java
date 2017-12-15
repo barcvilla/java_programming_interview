ee809e2df3a6fbe4d2cb2e3f05854c5101a0ec49
Carlos Villanueva <ceva_19@hotmail.com>
2017-12-15 08:25:45 -0300

    Comparing Two int

--- java_programming_interview/src/ch03/comparing/ex1/ComparingTwoInt.java
+++ java_programming_interview/src/ch03/comparing/ex1/ComparingTwoInt.java
@@ -9,6 +9,39 @@ package ch03.comparing.ex1;
  *
  * @author barcvilla
  */
-public class ComparingTwoInt {
+public class ComparingTwoInt implements Comparable<Integer>
+{
+    private Integer first;
     
+    
+    public ComparingTwoInt(Integer first)
+    {
+        this.first = first;
+    }
+    
+    @Override
+    public int compareTo(Integer second)
+    {
+        if(this.first < second)
+        {
+            return -1;
+        }
+        if(this.first > second)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    public static void main(String[] args) 
+    {
+        Integer f = Integer.parseInt(args[0]);
+        Integer s = Integer.parseInt(args[1]);
+        
+        ComparingTwoInt c = new ComparingTwoInt(f);
+        int r = c.compareTo(s);
+        System.out.println("Los dos numeros son iguales: " + r);
+    }
 }
